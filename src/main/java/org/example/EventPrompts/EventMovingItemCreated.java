c673c45ddc0d0d1ddef5fc4679702e862faaedf3
Corinna <corinna.jasmin.sailer@hs-furtwangen.de>
2023-11-29 11:59:51 +0100

    Aufgabe 2: fixed id in EventMovingItemCreated

--- src/main/java/org/example/EventPrompts/EventMovingItemCreated.java
+++ src/main/java/org/example/EventPrompts/EventMovingItemCreated.java
@@ -3,11 +3,15 @@ package org.example.EventPrompts;
 import org.example.MovingItem.MovingItem;
 import org.example.MovingItem.MovingItemImpl;
 
+import java.util.Arrays;
+
 public class EventMovingItemCreated extends Event {
     public MovingItem item;
 
-    public EventMovingItemCreated(String name, int[] location, int value) {
+    public EventMovingItemCreated(String id, int[] location, int value) {
         super();
-        this.item = new MovingItemImpl(name, location, value);
+        this.id = id;
+        this.item = new MovingItemImpl(id, location, value);
+        System.out.println(item.getName() +":"+ Arrays.toString(item.getLocation()));
     }
 }
