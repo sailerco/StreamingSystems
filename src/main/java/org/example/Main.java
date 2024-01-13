caea7959eff16bd158645cce7ed758ae9f0ee4b9
Corinna <corinna.jasmin.sailer@hs-furtwangen.de>
2024-01-13 12:00:35 +0100

    feat: creates, moves, change value and delete event in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,13 +13,17 @@ public class Main {
         CommandImpl item = new CommandImpl();
         item.createItem("Tom", new int[]{1, 2, 3}, 0);
         item.moveItem("Tom", new int[]{2, 3, 4});
+        item.changeValue("Tom", 7);
         item.createItem("Lisa", new int[]{6, 7, 8}, 0);
-        item.createItem("Otto", new int[]{0, 0, 3}, 0);
+        item.deleteItem("Lisa");
+        item.createItem("Otto", new int[]{0, 0, 3}, 6);
 
         QueryHandler query = new QueryHandler();
         System.out.println("Item at position:");
         printMovingItems(query.getMovingItemsAtPosition(new int[]{0, 0, 3}));
-        System.out.println(query.getMovingItemByName("Tom").getName());
+        MovingItemDTO itemByName = query.getMovingItemByName("Tom");
+        if(itemByName != null)
+            System.out.println("Get item by name: " + itemByName.getName() + ", " + itemByName.getValue() + ", " + Arrays.toString(itemByName.getLocation()) + ", " + itemByName.getNumberOfMoves());
 
         System.out.println("All items:");
         printMovingItems(query.getMovingItems());
