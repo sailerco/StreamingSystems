769edc7fad2b55810d5e1c6c2a60edbb8d1b6d06
Janina <feder.tinte@gmail.com>
2024-01-23 14:51:07 +0100

    todo entfernt

--- src/main/java/org/example/QuerySide/QueryModel.java
+++ src/main/java/org/example/QuerySide/QueryModel.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 
 public class QueryModel {
-    //Represents a database of MovingItemDTO objects accessible for querying. The Event Handler saves items into this database, and the query side can retrieve items from it.
+    //Represents a database of MovingItemDTO objects accessible for querying.
+    // The Event Handler saves items into this database, and the query side can retrieve items from it.
     public static Map<String, MovingItemDTO> query_database = new HashMap<>();
 }
