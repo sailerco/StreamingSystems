4bed737cc5fcdc56aa60b3e725e7bc034de6c359
Corinna Sailer <corinna.jasmin.sailer@hs-furtwangen.de>
2024-02-04 23:05:41 +0100

    deletetd comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,7 +28,6 @@ public class Main {
     );
 
     public static void main(String[] args) throws Exception {
-        //TODO: Update the Tests
         env.start(); //for embedded
         EventHandler eventHandler = new EventHandler();
 
